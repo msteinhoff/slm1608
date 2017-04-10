3ac17cf1d22ac98a7e256e9b910e5e913184eec2
Mario Steinhoff <steinhoff.mario@gmail.com>
2017-04-10 05:55:34 +0200

    Change controller to wait for controller reset and write simple data

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -2,47 +2,31 @@ import sys
 import serial
 import time
 
-framebuffer = bytearray([0] * 1024)
-counter = 0
-
-def updateFramebuffer():
-    global framebuffer
-    global counter
-    framebuffer[counter] = 0x01
-    counter += 1
-    if counter > 1024:
-        framebuffer = bytearray([0] * 1024)
-        counter = 0
-
-def printFramebuffer():
-    print "hi"
-    for index, byte in enumerate(framebuffer):
-        print byte,
-        if (index > 0 and index % 64 == 0):
-            print "x"
-
-def writeFramebuffer(ser):
-    for byte in framebuffer:
-        ser.write(byte)
-
 if __name__ == "__main__":
-    #/dev/tty.usbmodem641
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print "usage: controller.py [serialdevice]"
         sys.exit(-1)
 
-    print "printing pixels to %s" % (sys.argv[1])
-    with serial.Serial(sys.argv[1], 38400, timeout=1) as ser:
-        while True:
-            # update framebuffer
-            updateFramebuffer()
-            print "dot"
+    port = sys.argv[1]
+    baud = int(sys.argv[2])
+    print "Connecting to %s with %s baud" % (port, baud)
 
-            printFramebuffer()
+    with serial.Serial(port, baud , timeout=1, dsrdtr=True) as ser:
+        print "Connected to %s with %s baud" % (ser.port, ser.baudrate)
+        print "Waiting for controller to initialize..."
+        time.sleep(2)
 
-            # write framebuffer
-            writeFramebuffer(ser)
-            print "write"
+        print "Reading welcome message:"
+        print ser.read(1024)
 
-            # limit framerate
-            time.sleep(0.1)
+        print "Resetting framebuffer:"
+        ser.write(b'i')
+        print "Result:"
+        print ser.read(1024)
+
+        print "Writing green pixels:"
+        for i in range(1024):
+            ser.write(b'g')
+            ser.flush()
+            time.sleep(0.01)
+        print "Done"
